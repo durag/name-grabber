159f790b03b2fbdd5a1e6650b3e56b142b76bf58
Will Moritz <will@crowdfavorite.com>
2015-10-26 19:42:43 -0400

    basic logic

--- index.js
+++ index.js
@@ -10,27 +10,71 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+// app.get('/', function(request, response) {
+//   response.render('pages/index');
+// });
+
+app.get('/', function (req, res, next) {
+  // If this is a user query, check for user
+  if (req.query.username) {
+    var username = req.query.username;
+    var message = "That username is available, register through the Instagram app.";
+
+    checkForUsername(username, function(exists) {
+      if (exists) {
+        // Prompt for email
+        console.log("user exists");
+        res.render('pages/index', {
+          response: 'user exists'
+        });
+      } else {
+        // Redirect to register on Instagram
+        console.log("user NO exists");
+        res.render('pages/index', {
+          response: 'user does not exist'
+        });
+
+      }
+    });
+  // or just load the home page
+  } else {
+    res.render('pages/index', {
+      response: 'nothing'
+    });
+  }
 });
 
-app.get('/username/:id', function (req, res, next) {
-  var username = req.params.id;
-  var message = "That username is available, register through the Instagram app.";
+// Handles email submissions
+app.post('/email', function(req, res, next) {
+  console.log(req.params.email);
+
+  // Add email to database
+
+});
+
+// Check Instagram to see if user exists
+function checkForUsername(username, cb) {
   var url = "https://www.instagram.com/" + username;
-  // Check Instagram for username
+  var exists = false;
   request(url, function(err, res, body) {
     if (res.statusCode === 200) {
-      // If the unsername is taken
-      message = "Username taken, enter email to be notified when it frees up.";
+      // If the unsername is taken, update message and ask for user's email
+      exists = true;
     }
-    console.log(message);
+    cb(exists);
   });
-  res.send("Whee");
-  // res.render('pages/username', {
-  //   response: parsed
-  // });
-});
+}
+
+// Check database 2x / day
+function checkDatabase() {
+  for (var i = 0; i < usernames.length; i++) {
+    checkForUsernames(usernames[i], function(exists) {
+      if(exists) {
+        // if exists, email users email and remove this userns email from the database
+      }
+    })
+  }
+}
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
